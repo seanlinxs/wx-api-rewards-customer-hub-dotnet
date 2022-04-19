d1147449670f9cf54c1c8ea5ce8abb36840f1557
Sean Lin <xlin@woolworths.com.au>
2022-04-19 13:47:14 +1000

    Initial commit

---
+++ Models/Member.cs
@@ -0,0 +1,11 @@
+namespace wx_api_rewards_customer_hub.Models;
+
+public class member
+{
+    public string CRN { get; set; }
+    public string CardNumber { get; set; }
+    public string Email { get; set; }
+    public string Mobile { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
