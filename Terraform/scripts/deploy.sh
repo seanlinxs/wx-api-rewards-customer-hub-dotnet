ffc083ad467158c9f45d0e39f20edfec7e48944d
Sean Lin <xlin@woolworths.com.au>
2022-06-09 17:07:27 +1000

    Add terraform and run init without issue

---
+++ Terraform/scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+readonly service="$1"
+readonly server_to_run="$2"
+readonly project_id="$3"
+
+gcloud run deploy "$service-$server_to_run" \
+    --image "gcr.io/$project_id/$service" \
+    --region australia-southeast1 \
+    --platform managed
---
+++ Terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "project" {}
+variable "user" {}
+variable "region" {}
+
+variable "billing_account" {
+  description = "Billing account display name"
+}
+
+variable "repository_name" {
+  default = "wx-api-rewards-customer-hub"
+}
