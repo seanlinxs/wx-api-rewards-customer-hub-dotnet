af8ead0abb1551c7dcbb1032fc7698680943d9a5
Sean Lin <xlin@woolworths.com.au>
2022-06-10 11:48:32 +1000

    Fix docker image run path

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,9 +2,5 @@
 readonly service="$1"
 readonly project_id="$2"
 
-PROJECT_ROOT=..
-
-rm -fr $PROJECT_ROOT/obj
-rm -fr $PROJECT_ROOT/bin
-docker build -t "gcr.io/$project_id/$service" $PROJECT_ROOT -f $PROJECT_ROOT/Dockerfile --build-arg "SERVICE=$service"
+docker build -t "gcr.io/$project_id/$service" . -f ./Dockerfile --build-arg "SERVICE=$service"
 docker push "gcr.io/$project_id/$service"
